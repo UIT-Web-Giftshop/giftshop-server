67c445e79415ec7cbd53270d7b719d5b902113d2
Hung Nguyen <gaughegomth01@gmail.com>
2022-03-10 11:16:26 +0700

    fix hardcode collectionName

--- Infrastructure/Repositories/ProductRepository.cs
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -2,6 +2,7 @@
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
+using Domain.Attributes;
 using Domain.Entities;
 using Infrastructure.Context;
 using Infrastructure.Interfaces;
@@ -21,14 +22,31 @@ namespace Infrastructure.Repositories
             var addedProduct = await base.AddAsync(entity, cancellationToken);
             if (addedProduct == null) 
                 return null;
-            
-            Expression<Func<SaveFlag, bool>> expression = x => x.CollectionName == "products";
+
+            #region Increase product counts
+
+            Expression<Func<SaveFlag, bool>> expression = x => x.CollectionName == BsonCollection.GetCollectionName<Product>();
             var productCounts = await _saveFlagRepository.GetOneAsync(expression, cancellationToken);
-            await _saveFlagRepository.PatchOneFieldAsync(
-                expression, 
-                p => p.CurrentCount,
-                productCounts.CurrentCount + 1,
-                cancellationToken);
+            if (productCounts != null)
+            {
+                await _saveFlagRepository.PatchOneFieldAsync(
+                    expression,
+                    p => p.CurrentCount,
+                    productCounts.CurrentCount + 1,
+                    cancellationToken);
+            }
+            else
+            {
+                // auto create document if not exist
+                var productFlag = new SaveFlag()
+                {
+                    CollectionName = BsonCollection.GetCollectionName<Product>(),
+                    CurrentCount = 1
+                };
+                await _saveFlagRepository.AddAsync(productFlag, cancellationToken);
+            }
+            
+            #endregion
 
             return addedProduct;
         }
