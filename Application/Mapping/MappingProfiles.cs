d8564d6899430cb48086b083f6c4d3f7cb7dea4e
hung.nguyen <gaughegomth01@gmail.com>
2022-03-30 12:10:21 +0700

    auth signup feature

--- Application/Mapping/MappingProfiles.cs
+++ Application/Mapping/MappingProfiles.cs
@@ -1,4 +1,5 @@
-﻿using Application.Features.Orders.Vms;
+﻿using Application.Features.Auths.SignupUser;
+using Application.Features.Orders.Vms;
 using Application.Features.Products.Vms;
 using Application.Features.Users.Vms;
 using AutoMapper;
@@ -14,6 +15,7 @@ namespace Application.Mapping
             CreateMap<Product, ProductVm>().ReverseMap();
             CreateMap<User, UserVm>().ReverseMap();
             CreateMap<Order, OrderVm>().ReverseMap();
+            CreateMap<SignupUserCommand, User>().ReverseMap();
         }
     }
 }
