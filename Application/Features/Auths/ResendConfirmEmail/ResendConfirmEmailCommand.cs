87a399072ee2b205026acca243d256ea778422c8
hungw7 <gaughegomth01@gmail.com>
2022-06-08 12:14:20 +0700

    update verify

---
+++ Application/Features/Auths/ResendConfirmEmail/ResendConfirmEmailCommand.cs
@@ -0,0 +1,21 @@
+﻿using Application.Commons;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Features.Auths.ResendConfirmEmail
+{
+    public class ResendConfirmEmailCommand : IRequest<ResponseApi<Unit>>
+    {
+        public string Email { get; set; }
+    }
+
+    public class ResendConfirmEmailCommandValidator : AbstractValidator<ResendConfirmEmailCommand>
+    {
+        public ResendConfirmEmailCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email không được trống")
+                .EmailAddress().WithMessage("Email không hợp lệ");
+        }
+    }
+}
---
+++ Infrastructure/Extensions/GuidExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Infrastructure.Extensions
+{
+    public static class GuidExtension
+    {
+        public static bool IsGuid(this string guid)
+        {
+            return Guid.TryParse(guid, out _);
+        }
+    }
+}
+
+
+
+
