130829c80803558d27333d8f2309f554d744dd32
hungw7 <gaughegomth01@gmail.com>
2022-06-09 22:34:42 +0700

    missing file

---
+++ RazorEmailLibs/Views/Emails/ResetPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RazorEmailLibs.Views.Emails
+{
+    public class ResetPasswordViewModel
+    {
+        public ResetPasswordViewModel(string url)
+        {
+            Url = url;
+        }
+
+        public string Url { get; set; }
+    }
+}
