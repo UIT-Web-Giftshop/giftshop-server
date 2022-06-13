459f259e264678b0a4594397b8937c8bf91617fd
hungw7 <gaughegomth01@gmail.com>
2022-06-13 08:55:45 +0700

    update

--- API/wwwroot/js/chunk-2d21ad47.e9d7d492.js
+++ API/wwwroot/js/chunk-2d21ad47.e9d7d492.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21ad47"],{bcd4:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",[t._v(" "+t._s(this.$route.params.test2)+" ")])},c=[],o={created:function(){console.log(this.$router)}},r=o,u=n("2877"),a=Object(u["a"])(r,s,c,!1,null,null,null);e["default"]=a.exports}}]);
-//# sourceMappingURL=chunk-2d21ad47.f0536d78.js.map
+//# sourceMappingURL=chunk-2d21ad47.e9d7d492.js.map
