65015c6f13799bcfc238dcb5ebdb8b9504dc24ec
Hung Nguyen <hungntsol77@gmail.com>
2022-12-27 19:03:00 +0700

    add certs

--- API/wwwroot/js/chunk-2d21ad47.e9d7d492.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21ad47"],{bcd4:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",[t._v(" "+t._s(this.$route.params.test2)+" ")])},c=[],o={created:function(){console.log(this.$router)}},r=o,u=n("2877"),a=Object(u["a"])(r,s,c,!1,null,null,null);e["default"]=a.exports}}]);
-//# sourceMappingURL=chunk-2d21ad47.e9d7d492.js.map
